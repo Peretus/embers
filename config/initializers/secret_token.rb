ed0866a733558f04b9a1f2ddb6890c60a3812388
Kelley Rytlewski <students@themakersquare.com>
2014-01-14 13:06:50 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Remory::Application.config.secret_key_base = '1a92b42b6e5e2015eed70aaaffd5b175f725655f03038d0ca1bd177f5e832e3b71fbd49aab11d018dd15fc24e9ac5892840830725b6ee29488e4fb51421b06e8'
