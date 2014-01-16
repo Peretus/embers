ebae6cb6856783b361dde5d04abad4d96caf1154
Casey McNeil <CaseyMcNeil@gmail.com>
2014-01-16 15:46:11 -0600

    Added model method to prepare data before passing it to js

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,6 @@ class PagesController < ApplicationController
   def welcome
     @fact = Path.first.decks.first.facts.first
     @words = @fact.split_to_answer_objects
+  
   end
 end
--- app/models/fact.rb
+++ app/models/fact.rb
@@ -5,7 +5,22 @@ class Fact < ActiveRecord::Base
 
 
   def split_to_answer_objects 
-    self.definition.split(' ')
+    spaced_word = self.definition.gsub('.', " . ")
+    spaced_word.split(' ')
   end
+
+  def shuffled_answer_objects
+  end
+
+  def correct_answer? (answer, response)
+      
+      
+    concat_response = response.join(" ")
+    normalized_response = concat_response.gsub(' . ', '.')
+    normalized_response == answer
+  end
+
+
+
 
 end
