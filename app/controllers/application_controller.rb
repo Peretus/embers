3b95f86501f3e6a694f1d21accdb96f37a240539
Kelley Rytlewski <students@themakersquare.com>
2014-01-15 21:19:20 -0600

    Add ability to move all choices to new container and back to original.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action  :check_login
+  # before_action  :check_login
 
-  def check_login
-    unless user_signed_in?
-      redirect_to pages_welcome
-    # if @user
-    #   redirect_to user_paths @user
-    # else
-    #   redirect_to pages_welcome
-    # end
-    end
-  end
+  # def check_login
+  #   unless user_signed_in?
+  #     redirect_to pages_welcome
+  #   # if @user
+  #   #   redirect_to user_paths @user
+  #   # else
+  #   #   redirect_to pages_welcome
+  #   # end
+  #   end
+  # end
 end
