1f7b8f0b1ac0da8cd1ad66558281fe288e852d71
Kelley Rytlewski <kelley.rytlewski@gmail.com>
2014-01-24 15:10:51 -0600

    Both google and Fb omniauth working. Corrected scheme.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   has_many :decks
   has_many :enrollments
   has_many :paths, through: :enrollments
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124140941) do
+ActiveRecord::Schema.define(version: 20140124210841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
